ddf7902729ebe8f343ae146518076d810133af2e
SadandMad <63254457+SadandMad@users.noreply.github.com>
2020-04-06 20:40:09 +0300

    Add files via upload

---
+++ Plugins/CGeometric/Geometric.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using IGeometric;
+
+namespace CGeometric
+{
+    [Serializable]
+    public abstract class Geometric : GeometricInterface
+    {
+        public string Name;
+        public int x1 = 0;
+        public int y1 = 0;
+        public int x2 = 0;
+        public int y2 = 0;
+        public Color Pclr = Color.Black;
+        public Color Fclr = Color.Transparent;
+        public abstract void Draw(PictureBox picture);
+    }
+}
---
+++ Plugins/CLine/Line.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CGeometric;
+
+namespace CLine
+{
+    [Serializable]
+    public class Line : Geometric
+    {
+        public Line(int c1, int c2, int c3, int c4, Color Color1, Color Color2)
+        {
+            x1 = c1;
+            x2 = c3;
+            y1 = c2;
+            y2 = c4;
+            Pclr = Color1;
+        }
+        public Line(int c1, int c2, int c3, int c4)
+        {
+            x1 = c1;
+            x2 = c3;
+            y1 = c2;
+            y2 = c4;
+        }
+        public Line()
+        {
+
+        }
+        override public void Draw(PictureBox picture)
+        {
+            Graphics elem = picture.CreateGraphics();
+            Pen pen = new Pen(Pclr);
+            elem.DrawLine(pen, x1, y1, x2, y2);
+        }
+    }
+}
