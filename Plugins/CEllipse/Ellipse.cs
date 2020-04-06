49621021c5ba03ec1d8acd63d98c8252b99e84e3
SadandMad <63254457+SadandMad@users.noreply.github.com>
2020-04-06 20:37:53 +0300

    Add files via upload

---
+++ Plugins/CEllipse/Ellipse.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CGeometric;
+
+namespace CEllipse
+{
+    [Serializable]
+    public class Ellipse : Geometric
+    {
+        public Ellipse(int c1, int c2, int c3, int c4, Color Color1, Color Color2)
+        {
+            x1 = c1;
+            x2 = c3;
+            y1 = c2;
+            y2 = c4;
+            Pclr = Color1;
+            Fclr = Color2;
+        }
+        public Ellipse(int c1, int c2, int c3, int c4)
+        {
+            x1 = c1;
+            x2 = c3;
+            y1 = c2;
+            y2 = c4;
+        }
+        public Ellipse()
+        {
+
+        }
+        override public void Draw(PictureBox picture)
+        {
+            Graphics elem = picture.CreateGraphics();
+            Pen pen = new Pen(Pclr);
+            SolidBrush brush = new SolidBrush(Fclr);
+            elem.DrawEllipse(pen, x1, y1, x2 - x1, y2 - y1);
+            elem.FillEllipse(brush, x1, y1, x2 - x1, y2 - y1);
+        }
+    }
+}
